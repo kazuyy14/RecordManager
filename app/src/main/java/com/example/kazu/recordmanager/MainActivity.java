f4bfa3458753fd0c63515f36ae4369cdc09696ed
Kazuki Nakano <kazuyy14xavi@gmail.com>
2017-11-07 12:58:20 +0900

    loadlistとsavelistの記述

--- app/src/main/java/com/example/kazu/recordmanager/MainActivity.java
+++ app/src/main/java/com/example/kazu/recordmanager/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.kazu.recordmanager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import org.json.JSONArray;
 import java.util.ArrayList;
 
 
@@ -13,6 +16,34 @@ public class MainActivity extends AppCompatActivity {
     ListView myListView;
     ArrayList<String> items = new ArrayList<String>();
 
+    public static ArrayList<String> loadList(Context ctx, String key) {
+        ArrayList<String> items = new ArrayList<String>();
+        SharedPreferences prefs = ctx.getSharedPreferences("HisList", Context.MODE_PRIVATE);
+        String strJson = prefs.getString(key, "");
+        if(!strJson.equals("")) {
+            try {
+                JSONArray jsonAry = new JSONArray(strJson);
+                for(int i=0; i<jsonAry.length(); i++) {
+                    items.add(jsonAry.getString(i));
+                }
+            } catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return items;
+    }
+
+    public static void saveList(Context ctx, String key, ArrayList<String> items) {
+        JSONArray jsonAry = new JSONArray();
+        for(int i=0; i<items.size(); i++) {
+            jsonAry.put(items.get(i));
+        }
+        SharedPreferences prefs = ctx.getSharedPreferences("HisList", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, jsonAry.toString());
+        editor.apply();
+    }
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
                 items.add("value");
                 arrayAdapter.notifyDataSetChanged();
+
 
             }
         });
